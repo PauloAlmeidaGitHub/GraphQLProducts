3e8afffbb733ddc99db486786efbbb9242ff6a41
Paulo Cezar de Almeida <pauloalmeida.rj@hotmail.com>
2021-11-23 09:36:35 -0300

    Last version

--- GraphQLProject/Services/ProductService.cs
+++ GraphQLProject/Services/ProductService.cs
@@ -66,7 +66,7 @@ namespace GraphQLProject.Services
             }
             return objectList;
             /*
-            {
+                {
                   products{
                     id, name, price
                   }
@@ -80,21 +80,21 @@ namespace GraphQLProject.Services
         {
             //products.Add(product); 
             //return product;
-           
+
 
             _dbContext.Products.Add(product);
             _dbContext.SaveChanges();
 
             // ALIMENTA O CACHE
-                objectList = _dbContext.Products.ToList();
-                serializedObjectList = JsonConvert.SerializeObject(objectList);
-                var redisProductList = Encoding.UTF8.GetBytes(serializedObjectList);
+            objectList = _dbContext.Products.ToList();
+            serializedObjectList = JsonConvert.SerializeObject(objectList);
+            var redisProductList = Encoding.UTF8.GetBytes(serializedObjectList);
 
-                // DEFINE PRAZO DE EXPIRACAO PARA O CACHE (10min Janela Expiração 2min)
-                var options = new DistributedCacheEntryOptions().SetAbsoluteExpiration(DateTime.Now.AddMinutes(10)).SetSlidingExpiration(TimeSpan.FromMinutes(2));
+            // DEFINE PRAZO DE EXPIRACAO PARA O CACHE (10min Janela Expiração 2min)
+            var options = new DistributedCacheEntryOptions().SetAbsoluteExpiration(DateTime.Now.AddMinutes(10)).SetSlidingExpiration(TimeSpan.FromMinutes(2));
 
-                _distributedCache.SetString(key, serializedObjectList);
-            
+            _distributedCache.SetString(key, serializedObjectList);
+
             return product;
         }
         /*
@@ -123,11 +123,42 @@ namespace GraphQLProject.Services
 
             // var productObjAUX = _dbContext.Products.FirstOrDefault(p => p.Id == id);  //TESTAR
             var productObjAUX = _dbContext.Products.Find(id);
-
             productObjAUX.Name = product.Name;
             productObjAUX.Price = product.Price;
-
             _dbContext.SaveChanges();
+
+            //OBTEM OBJECT LIST PELO CACHE OU PELO BANCO
+            var cache = _distributedCache.GetString(key);
+            if (cache != null)
+            {
+                // OBTEM DADOS NO CACHE
+                var redisObjectList = _distributedCache.Get(key);
+                serializedObjectList = Encoding.UTF8.GetString(redisObjectList);
+                objectList = JsonConvert.DeserializeObject<List<Product>>(serializedObjectList);
+            }
+            else
+            {
+                // ALIMENTA O CACHE
+                objectList = _dbContext.Products.ToList();
+            }
+
+            // UPDATE NO OBJETO EM MEMÓRIA
+            for (int i = 0; i < objectList.Count; i++)
+            {
+                if (product.Id == objectList[i].Id)
+                {
+                    objectList[i].Name = product.Name;
+                    objectList[i].Price = product.Price;
+                    break;
+                }
+            }
+
+            // PREPARA ENVIO PARA O CACHE
+            serializedObjectList = JsonConvert.SerializeObject(objectList);
+            var options = new DistributedCacheEntryOptions().SetAbsoluteExpiration(DateTime.Now.AddMinutes(10)).SetSlidingExpiration(TimeSpan.FromMinutes(2));
+
+            _distributedCache.SetString(key, serializedObjectList);
+
             return product;
         }
         /*
@@ -159,6 +190,42 @@ namespace GraphQLProject.Services
             var productObjAUX = _dbContext.Products.Find(id);
             _dbContext.Remove(productObjAUX);
             _dbContext.SaveChanges();
+
+            //OBTEM OBJECT LIST PELO CACHE OU PELO BANCO
+            var cache = _distributedCache.GetString(key);
+            if (cache != null)
+            {
+                // OBTEM DADOS NO CACHE
+                var redisObjectList = _distributedCache.Get(key);
+                serializedObjectList = Encoding.UTF8.GetString(redisObjectList);
+                objectList = JsonConvert.DeserializeObject<List<Product>>(serializedObjectList);
+            }
+            else
+            {
+                // ALIMENTA O CACHE
+                objectList = _dbContext.Products.ToList();
+            }
+
+            //// DELETE NO OBJETO EM MEMÓRIA
+            //for (int i = 0; i < objectList.Count; i++)
+            //{
+            //    if (id == objectList[i].Id)
+            //    {
+            //        objectList.RemoveAt(i);
+            //        break;
+            //    }
+
+            //}
+
+            // DELETA NA LISTA
+            objectList.RemoveAt(id);
+
+            // PREPARA ENVIO DA LISTA PARA O CACHE
+            serializedObjectList = JsonConvert.SerializeObject(objectList);
+            var options = new DistributedCacheEntryOptions().SetAbsoluteExpiration(DateTime.Now.AddMinutes(10)).SetSlidingExpiration(TimeSpan.FromMinutes(2));
+
+            // SALVA LISTA NO CACHE
+            _distributedCache.SetString(key, serializedObjectList);
         }
         /*
             mutation DeleteProduct($id:Int){
@@ -170,12 +237,32 @@ namespace GraphQLProject.Services
             }
         */
 
+        //===================================================================================
         public Product GetProductById(int id)
         {
-            //return products.Find(p => p.Id == id);
+            Product product = new();
 
-            // return _dbContext.Products.FirstOrDefault(p => p.Id == id);    // Testar - Nesse caso vazio não vai gerar exception
-            return _dbContext.Products.Find(id);
+            var cache = _distributedCache.GetString(key);
+            if (cache != null)
+            {
+                // OBTEM DADOS NO CACHE
+                var redisObjectList = _distributedCache.Get(key);
+                serializedObjectList = Encoding.UTF8.GetString(redisObjectList);
+                objectList = JsonConvert.DeserializeObject<List<Product>>(serializedObjectList);
+                product = objectList.FirstOrDefault(p => p.Id == id);
+            }
+            else
+            {
+                // OBTEM DADOS NO BANCO
+                //Product product = _dbContext.Products.Find(id);
+                product = _dbContext.Products.FirstOrDefault(p => p.Id == id);
+            }
+
+            return product;
+
         }
+        /*
+             {product(id: 10) { id, name, price }}
+        }*/
     }
 }
